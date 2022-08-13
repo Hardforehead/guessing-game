a1e0e92293aedbd9611f8525851e80c98dd9bd68
Hardforehead <kochegar005@gmail.com>
2022-08-13 21:24:21 +0300

    feat: solution added

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.a;
+        this.b;
+    }
     setRange(min, max) {
-
+        this.a = min;
+        this.b = max;
     }
-
     guess() {
-
+        this.c = (this.b - this.a) / 2;
+        this.d = Math.ceil(this.c);
+        this.e = Math.floor(this.c);
+        return (this.a + this.d);
     }
-
     lower() {
-
+        this.b = this.b - this.e;
     }
-
     greater() {
-
+        this.a = this.a + this.d;
     }
 }
 
